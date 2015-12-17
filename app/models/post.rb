9827910b194ec1937a877f311a73b85214f78108
thienhv <thien.hoviet@gmail.com>
2015-12-17 17:01:03 +0700

    index post, add timestamp for Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,10 @@
 class Post
   include Mongoid::Document
+  include Mongoid::Timestamps
+
   field :title, type: String
   field :body, type: String
   field :starred, type: Mongoid::Boolean
+
+  index({ starred: 1 })
 end
