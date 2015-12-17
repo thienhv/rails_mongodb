bf4619e19fdab59e4b2c27090ed75930a526f9ca
thienhv <thien.hoviet@gmail.com>
2015-12-17 17:12:42 +0700

    add comments for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@ class Post
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  embeds_many :comments
+
   field :title, type: String
   field :body, type: String
   field :starred, type: Mongoid::Boolean
